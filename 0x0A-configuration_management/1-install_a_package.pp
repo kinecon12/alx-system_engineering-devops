a20a0e1e037d2b39706f8fe36f1e79dc90d9905a
kinecon12 <kinecon12@gmail.com>
2024-04-20 03:41:59 -0700

    update falsk installatio

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'], # Add any additional paths where pip3 might be installed
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"', # Checks if Flask 2.1.0 is already installed
+  require => Package['python3-pip'],         # Ensures python3-pip is installed before attempting to install Flask
 }
+
