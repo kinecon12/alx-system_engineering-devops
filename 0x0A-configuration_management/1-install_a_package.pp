d1275403126714d70402b289c8359a3105210d3d
kinecon12 <kinecon12@gmail.com>
2024-04-20 03:45:04 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'], # Add any additional paths where pip3 might be installed
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"', # Checks if Flask 2.1.0 is already installed
-  require => Package['python3-pip'],         # Ensures python3-pip is installed before attempting to install Flask
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
 
